8f23d8c24a682b7439cc9d9c990e30b3ba5c1542
Clement Labrugere <clabrugere@gmail.com>
2025-02-04 22:29:08 +0100

    clunky caching now works

--- src/policies/rng.rs
+++ src/policies/rng.rs
@@ -1,35 +1,28 @@
 use rand::{rngs::SmallRng, SeedableRng};
-use serde::{Deserialize, Deserializer, Serialize};
+use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Serialize)]
+#[derive(Debug, Serialize, Deserialize)]
 pub struct MaybeSeededRng {
     seed: Option<u64>,
     #[serde(skip)]
+    #[serde(default = "default_rng")]
     rng: SmallRng,
+}
+
+fn default_rng() -> SmallRng {
+    SmallRng::from_entropy()
 }
 
 impl MaybeSeededRng {
     pub fn new(seed: Option<u64>) -> Self {
-        let rng = if let Some(seed) = seed {
-            SmallRng::seed_from_u64(seed)
-        } else {
-            SmallRng::from_entropy()
+        let rng = match seed {
+            Some(seed) => SmallRng::seed_from_u64(seed),
+            None => SmallRng::from_entropy(),
         };
-
         Self { seed, rng }
     }
 
     pub fn get_rng(&mut self) -> &mut SmallRng {
         &mut self.rng
-    }
-}
-
-impl<'de> Deserialize<'de> for MaybeSeededRng {
-    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-    where
-        D: Deserializer<'de>,
-    {
-        let seed = Deserialize::deserialize(deserializer)?;
-        Ok(Self::new(seed))
     }
 }
